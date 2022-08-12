fb58ac262498e4c3cb0dd513bcb89798b91defab
Ronaldo Suarez Surface <ronaldo3055@gmail.com>
2022-08-12 07:41:00 -0500

    Added colors and styling to size chart

--- src/components/SizesDD.js
+++ src/components/SizesDD.js
@@ -7,37 +7,37 @@ const SizesDD = () => {
         <div className="dropdown-wrapper">
           <div className="dropdown-sizechart">
               <span>Select Size</span>
-              <span>Size Chart</span>
+              <span className="size-chart">Size Chart</span>
           </div>
           <div className="dropdown-all">
             <button>
-              <span>All</span>
+              <span className="shoe-size-title">All</span>
               <span>$237</span>
             </button>
           </div>
           <div className="dropdown-sizes">
             <button>
-              <span>US M 7</span>
+              <span className="shoe-size-title">US M 7</span>
               <span>$237</span>
             </button>
             <button>
-              <span>US M 8</span>
+              <span className="shoe-size-title">US M 8</span>
               <span>$237</span>
             </button>
             <button>
-              <span>US M 9</span>
+              <span className="shoe-size-title">US M 9</span>
               <span>$237</span>
             </button>
             <button>
-              <span>US M 10</span>
+              <span className="shoe-size-title">US M 10</span>
               <span>$237</span>
             </button>
             <button>
-              <span>US M 11</span>
+              <span className="shoe-size-title">US M 11</span>
               <span>$237</span>
             </button>
             <button>
-              <span>US M 12</span>
+              <span className="shoe-size-title">US M 12</span>
               <span>$237</span>
             </button>
           </div>
